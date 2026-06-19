0049e797580777a583e647874df16f541dcd6747
Danie <daniewang1113@gmail.com>
2026-06-19 18:55:22 +0800

    Initial Commit

---
+++ HuahuaDiary/HuahuaDiaryApp.swift
@@ -0,0 +1,17 @@
+//
+//  HuahuaDiaryApp.swift
+//  HuahuaDiary
+//
+//  Created by 汪画 on 2026/6/19.
+//
+
+import SwiftUI
+
+@main
+struct HuahuaDiaryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
